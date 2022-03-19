4fbcba22e7b5badb72e4ad9b93adab4d7ce4fb01
brian <otienolubalo@yahoo.com>
2022-03-20 02:55:32 +0300

    adding yes or no button

--- script/script.js
+++ script/script.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
         var checkout_x = document.getElementById("checkoutbutton");
         checkout_x.style.display = "block";
         
+
     
         $('#thead').append(`<tr id="R${rowIdx}">
             <th class="text-center">
@@ -96,62 +97,35 @@ $(document).ready(function () {
 
             </tr>`);
     });
+    $('#checkoutbutton').on('click', function () {
+        var x = document.getElementById("checkoutbutton");
+        x.style.display = "none";
+
+        var order_x = document.getElementById("addBtnAnother");
+        order_x.style.display = "none";
+        
+        var totalcost_x = document.getElementById("cost_disply");
+        totalcost_x.style.display = "block";
+        
+    })
+    $('#yes').on('click', function () {
+        var x = document.getElementById("yes");
+        x.style.display = "none";
+
+        var order_x = document.getElementById("no");
+        order_x.style.display = "none";
+        
+    })
+    $('#no').on('click', function () {
+        var x = document.getElementById("no");
+        x.style.display = "none";
+
+        var order_x = document.getElementById("yes");
+        order_x.style.display = "none";
+        
+    })
     });
 
 
 
-
-function popupateTable(){
-    
-    var crust = document.getElementById("crust").value; 
-    var topping = document.getElementById("toppings").value; 
-    var size = document.getElementById("size").value; 
-
-    size_prices = {small:500,medium:750,large:1000}
-    toppings_prices = {olives:100,pepperon:400,cheese:450}
-    crust_prices ={crispy:400,gluten_free:250,shufferd:300}
-     
-     
-     var total_cost = size_prices[size] + toppings_prices[topping] + crust_prices[crust]
-    
-
-    var table = document.createElement('table');
-
-
-    var tr = document.createElement('tr');   
-
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var td4 = document.createElement('td');
-    var td5 = document.createElement('td');
-
-    var text1 = document.createTextNode(1);
-    var text2 = document.createTextNode(size + '-' + size_prices[size]);
-    var text3 = document.createTextNode(crust+ '-' + toppings_prices[topping]);
-    var text4 = document.createTextNode(topping+ '-' + crust_prices[crust]);
-    var text5 = document.createTextNode("kshs" + total_cost);
-
-    td1.appendChild(text1);
-    td2.appendChild(text2);
-    td3.appendChild(text3);
-    td4.appendChild(text4);
-    td5.appendChild(text5);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-
-    table.appendChild(tr);
-    //}
-    document.body.appendChild(table);
-}
-var pizzaPrice = 0;
-
-function Pizza(size,toppings,pizzaPrice) {
-  this.size = size;
-  this.toppings = toppings;
-  this.pizzaPrice = 0;
-}
 
