89030a0d12f4329d122913876753d6075e34f6b9
brian <otienolubalo@yahoo.com>
2022-03-20 14:58:23 +0300

    modifying the buttons

--- script/script.js
+++ script/script.js
@@ -1,20 +1,35 @@
+function selected_order(){
+    var crust = document.getElementById("crust").value; 
+    var topping = document.getElementById("toppings").value; 
+    var size = document.getElementById("size").value; 
+    size_prices = {small:500,medium:750,large:1000}
+    toppings_prices = {olives:100,pepperon:400,cheese:300}
+    crust_prices ={crispy:400,gluten_free:250,shufferd:300} 
+    var total_cost = size_prices[size] + toppings_prices[topping] + crust_prices[crust]
+
+    return [crust, topping,size, toppings_prices[topping], size_prices[size], crust_prices[crust], total_cost]
+
+}
+
 $(document).ready(function () {
 
     // Denotes total number of rows
     var rowIdx = 0;
-    var crust = document.getElementById("crust").value; 
-    var topping = document.getElementById("toppings").value; 
-    var size = document.getElementById("size").value; 
+    var total_order_cost =0
 
-    size_prices = {small:500,medium:750,large:1000}
-    toppings_prices = {olives:100,pepperon:400,cheese:300}
-    crust_prices ={crispy:400,gluten_free:250,shufferd:300} 
-     
-    var total_cost = size_prices[size] + toppings_prices[topping] + crust_prices[crust]
-    
-    
     // jQuery button click event to add a row
     $('#addBtn').on('click', function () {
+        var orders = selected_order()
+        //[crust, topping,size, toppings_prices[topping], size_prices[size], crust_prices[crust], total_cost] =var orders = selected_order()
+        var crust = orders[0]
+        var topping = orders[1]
+        var size = orders[2]
+        var toppings_price = orders[3]
+        var size_price= orders[4]
+        var crust_price = orders[5]
+        var total_cost = orders[6]
+        total_order_cost = total_order_cost+total_cost
+        
         // Adding a row inside the tbody.
         var x = document.getElementById("addBtn");
         x.style.display = "none";
@@ -22,8 +37,6 @@ $(document).ready(function () {
         order_x.style.display = "block";
         var checkout_x = document.getElementById("checkoutbutton");
         checkout_x.style.display = "block";
-        
-
     
         $('#thead').append(`<tr id="R${rowIdx}">
             <th class="text-center">
@@ -54,15 +67,15 @@ $(document).ready(function () {
                   <p>${rowIdx}</p>
             </td>
             <td class="text-center">
-                <p> ${size}-${size_prices[size]} </p>
+                <p> ${size}-${size_price} </p>
             </td>
             <td class="text-center">
-            <p> ${topping}-${toppings_prices[topping]} </p>
+            <p> ${topping}-${toppings_price} </p>
             </td>
 
             </td>
             <td class="text-center">
-            <p> ${crust}-${crust_prices[crust]} </p>
+            <p> ${crust}-${crust_price} </p>
             </td>
             
             <td class="text-center">
@@ -75,6 +88,17 @@ $(document).ready(function () {
     // Add another order
     $('#addBtnAnother').on('click', function () {
         //var rowIdx = 1;
+        var orders = selected_order()
+        var crust = orders[0]
+        var topping = orders[1]
+        var size = orders[2]
+        var toppings_price = orders[3]
+        var size_price= orders[4]
+        var crust_price = orders[5]
+        var total_cost = orders[6]
+        total_order_cost= total_order_cost+total_cost
+        
+
         // Adding a row inside the tbody.
         $('#tbody').append(`<tr id="R${++rowIdx}">
             <td class="row-index text-center">
@@ -82,13 +106,13 @@ $(document).ready(function () {
             </td>
 
             <td class="text-center">
-                <p> ${size}-${size_prices[size]} </p>
+                <p> ${size}-${size_price} </p>
             </td>
             <td class="text-center">
-                <p> ${crust}-${crust_prices[crust]} </p>
+                <p> ${crust}-${crust_price} </p>
             </td>
             <td class="text-center">
-            <p> ${topping}-${toppings_prices[topping]} </p>
+            <p> ${topping}-${toppings_price} </p>
             </td>
             
             <td class="row-index text-center">
@@ -106,14 +130,17 @@ $(document).ready(function () {
         
         var totalcost_x = document.getElementById("cost_disply");
         totalcost_x.style.display = "block";
+        $("#totalCost").append(document
+            .createTextNode(total_order_cost));
+        console.log(total_order_cost)
         
     })
     $('#yes').on('click', function () {
         var x = document.getElementById("yes");
         x.style.display = "none";
-
         var order_x = document.getElementById("no");
         order_x.style.display = "none";
+        
         
     })
     $('#no').on('click', function () {
